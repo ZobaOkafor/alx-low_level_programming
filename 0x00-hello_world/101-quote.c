8ed6df46de0e1e868f9a4a4003c77aebcec5859a
ZobaOkafor <judithokafor30@gmail.com>
2023-09-07 15:53:50 -0700

    A C program that prints without puts or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 	const char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, text, sizeof(text) - 2);
+
+	write(2, text, sizeof(text) - 1);
 	return (1);
 }
