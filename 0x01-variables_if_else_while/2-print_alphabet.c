8f68ec5cb29fb57d03041bba98ab44d20fee3dbf
ZobaOkafor <judithokafor30@gmail.com>
2023-09-08 13:59:36 -0700

    Print alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main: This program prints alphabet in lowercase 
+ * main: This program prints alphabet in lowercase
  * followed by a new line
  *
  * Return: Always 0 (Success)
@@ -10,11 +10,11 @@
 int main(void)
 {
 	char c = 'a';
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
-       	}
+	}
 		putchar('\n');
 	return (0);
 }
