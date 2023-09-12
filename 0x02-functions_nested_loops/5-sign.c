b63f8137d0a6fb7883379fd10b1be2ce215512c3
ZobaOkafor <judithokafor30@gmail.com>
2023-09-12 12:09:47 -0700

    Print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,18 @@ int print_sign(int n)
 {	
 	if (n > 0)
 	{
-		return (1);
 		_putchar(+);
+		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
-		return (0);
 		_putchar(0);
+		return (0);
 	}
 	else
 	{
-		return (-1);
 		_putchar(-);
+		return (-1);
 	}
 	_putchar('\n');
 }
