59a7baa8b78206cfacebca07a68176e081ca1168
ZobaOkafor <judithokafor30@gmail.com>
2023-09-12 12:17:26 -0700

    Print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_sign - This function prints the sign ofma number.
+ * print_sign - This function prints the sign ofma number.
  *
  * @n: The character to check, in ASCII code.
  *
@@ -12,7 +12,7 @@ int print_sign(int n)
 {	
 	if (n > 0)
 	{
-		_putchar(+);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -22,7 +22,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar(-);
+		_putchar('-');
 		return (-1);
 	}
 	_putchar('\n');
