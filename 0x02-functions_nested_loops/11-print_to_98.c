4e698161db914b0bcefe66ddeb3d3596b85960dc
ZobaOkafor <judithokafor30@gmail.com>
2023-09-12 15:50:54 -0700

    Print all natural numbers from 0-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,20 @@ void print_to_98(int n)
 	if (n < 98) 
 	{
 		for (n = n; n < 98; n++)
-		_putchar('n');
-		_putchar(',');
-		_putchar( 98);
+		{
+			_putchar('n');
+			_putchar(',');
+			_putchar( 98);
+		}
 	}
 	else
 	{
 		for (n = n; n > 98; n--)
+		{
 			_putchar('n');
 			_putchar(',');
 			_putchar(98);
+		}
 	}
 	_putchar('\n');
 }
