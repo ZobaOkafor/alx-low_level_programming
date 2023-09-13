4e5427d060230a39a4d0c292d1c326a42210dbb7
ZobaOkafor <judithokafor30@gmail.com>
2023-09-13 10:58:20 -0700

    Print if an integer is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Function that prints if an integer is positive or negative.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	poditive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory houses tasks on debugging
