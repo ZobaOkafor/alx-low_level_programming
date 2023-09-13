3070ba06cd60aba947f1841630f06e9621de3a47
ZobaOkafor <judithokafor30@gmail.com>
2023-09-12 18:02:44 -0700

    Print the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - This program prints first 98 Fibonacci numbers,
+ * main - This program prints the first 98 Fibonacci numbers,
  * starting with 1 and 2,
  * separated by a comma, followed by a space.
  *
@@ -10,21 +10,40 @@
 
 int main(void)
 {
-        int count;
-        unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-        for (count = 0; count < 98; count++)
-        {
-                sum = fib1 + fib2;
-                printf("%lu", sum);
+	printf("%lu", bef);
 
-                fib1 = fib2;
-                fib2 = sum;
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
 
-                if (count == 97)
-                        printf("\n");
-                else
-                        printf(", ");
-        }
-        return (0);
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+	return (0);
 }
+
+
