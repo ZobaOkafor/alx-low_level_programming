5e45bb27f14f2c1ebee3aa1a3a44fb60b3ff5e1b
ZobaOkafor <judithokafor30@gmail.com>
2023-09-14 17:43:45 -0700

    Print a square

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,11 +16,17 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		while (n > 0)
+		int i, j;
+		for (i = 0; i < n; i++)
 		{
-			_putchar('\\');
-			n--;
-		}
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+				_putchar('\\');
+				else if (j < i)
+				_putchar(' ');
+			}
 		_putchar('\n');
+		}
 	}
 }
