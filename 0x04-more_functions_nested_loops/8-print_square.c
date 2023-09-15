7eaf9d5f39e13453903e43e78aaa4cd38c66e42c
ZobaOkafor <judithokafor30@gmail.com>
2023-09-14 17:37:27 -0700

    Print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,25 +10,21 @@
 
 void print_square(int size)
 {
-        if (size <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                int i = 0;
-		int j;
-		
-		while (i < size)
-                {
-			j = 0;
-			while (j < size)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
 			{
-                        	_putchar('#');
-                        	j++;
+				_putchar('#');
 			}
-                _putchar('\n');
-		i++;
-        	}	
+			_putchar('\n');
+		}
 	}
 }
