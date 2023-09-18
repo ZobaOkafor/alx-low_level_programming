d2946f571ed4bb8bae43d9523d74dc2916896012
ZobaOkafor <judithokafor30@gmail.com>
2023-09-18 14:44:48 -0700

    Copy string from source buffer to destination buffer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,13 +13,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	while (*src != '\0')
+	int i = 0;
+	
+	while (*src[i] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
+		i++;
 	}
-	*dest = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
