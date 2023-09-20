75ec237925a77a058915de08582ac3954df556f4
ZobaOkafor <judithokafor30@gmail.com>
2023-09-20 16:39:02 -0700

    Concatenate two strings using n bytes from source string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,11 +16,9 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 
-	while (src[j] != '\0' && n < 0)
+	while (src[j] != '\0' && n > 0)
 	{
 		dest[i] = src[j];
-		i++;
-		j++;
 		n--;
 	}
 	dest[i] = '\0';
