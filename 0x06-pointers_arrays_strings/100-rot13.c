f6546acb4d518ad594a9a5f8e987dda0f19c8562
ZobaOkafor <judithokafor30@gmail.com>
2023-09-21 09:33:21 -0700

    Encode a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,17 +12,19 @@ char *rot13(char *s)
 	int i;
 	int j;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwsyzabcdefghijklm";
+	char *a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwsyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
+		{
 			if (s[i] == a[j])
 			{
 				s[i] = b[j];
 				break;
 			}
+		}
 	}
 	return (s);
 }
