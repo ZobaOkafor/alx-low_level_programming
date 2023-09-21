b57c99abe2375c31f4996f514049a5c6167563b2
ZobaOkafor <judithokafor30@gmail.com>
2023-09-21 08:33:03 -0700

    Encode a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,10 +12,10 @@ char *leet(char *s)
 	int i;
 	int j;
 
-	char a[] = {'a', 'e', 'o', 't', 'l'};
-	char b[] = {'A', 'E', 'O', 'T', 'L'};
+	char a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char b[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	for (i = 0; s[i] != '\0', i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 			if (s[i] == a[j])
