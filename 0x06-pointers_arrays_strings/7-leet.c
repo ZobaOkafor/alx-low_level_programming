3a3f0d32112d2676d4f00edd43831f3163207849
ZobaOkafor <judithokafor30@gmail.com>
2023-09-21 08:24:43 -0700

    Encode a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - This function encodes a string into 1337
+ * @s: string
+ *
+ * Return: string encoded
+ */
+
+char *leet(char *s)
+{
+	int i;
+	int j;
+
+	char a[] = {'a','e','o','t','l'};
+	char b[] = {'A','E','O','T','L'};
+
+	for (i = 0; s[i] != '\0', i++)
+	{
+		for (j = 0; j < 10; j++)
+			if (s[i] == a[j])
+				s[i] = b[j];
+	}
+	return (s);
+}
