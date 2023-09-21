158b5a51d99cb2be5990fd1f0c8a37c52813038e
ZobaOkafor <judithokafor30@gmail.com>
2023-09-21 04:16:03 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1  == '\0' && *s2 == '\0')
-		return (0);
-	else if (*s1 == *s2)
-		return _strcmp(s1 + 1, s2 +1);
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+
+	if (*s1  == '\0' && *s2 != '\0')
+		return (-1);
+	else if (*s1 != '\0' && *s2 == '\0')
+		return (1);
 	else
-		return (*s1 - *s2);
+		return (0);
 }
