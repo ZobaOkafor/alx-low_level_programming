eb22dab02e6dc6dca398bdaf5425ec86b8e2ec78
ZobaOkafor <judithokafor30@gmail.com>
2023-09-25 09:33:24 -0700

    Copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,10 +16,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	i = 0;
 
 	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
-
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+		n--;
+	}
 
 	return (dest);
 }
