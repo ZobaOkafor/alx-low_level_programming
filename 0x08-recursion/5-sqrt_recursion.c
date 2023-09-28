0e680866480ed4c619117794ec46035348ee2288
ZobaOkafor <judithokafor30@gmail.com>
2023-09-28 15:23:58 -0700

    Return the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,10 +13,10 @@ int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
-	
+
 	else if (i * i == n)
 		return (i);
-	
+
 	return (actual_sqrt_recursion(n, i + 1));
 }
 
@@ -33,6 +33,6 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	
+
 	return (actual_sqrt_recursion(n, 0));
 }
