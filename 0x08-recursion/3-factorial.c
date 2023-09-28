d0de3a15b70f761f9ebe62626d1320fa3a19e537
ZobaOkafor <judithokafor30@gmail.com>
2023-09-28 14:37:25 -0700

    Return length of string

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,13 +7,11 @@
  * Return: factorial of n
  */
 
-int factorial(int n)   
+int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 
 	else
-		return (1);
-
-	return (n * factorial(n - 1));
+		return (n * factorial(n - 1));
 }
