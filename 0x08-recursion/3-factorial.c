ba2d0a66f0b594d06403c9fe566b5e00673ecfd3
ZobaOkafor <judithokafor30@gmail.com>
2023-09-28 14:30:40 -0700

    Return length of string

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - This function returns the factorial of a given number
+ * @n: factorial number
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)   
+{
+	if (n < 0)
+		return (-1);
+	
+	else	
+		return (1);
+		
+	return n * factorial(n - 1);
+}
