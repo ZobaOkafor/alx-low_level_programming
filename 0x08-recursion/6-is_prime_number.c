46181d4ab6df780fa72bd20d92611a697fa2a38a
ZobaOkafor <judithokafor30@gmail.com>
2023-09-28 15:44:15 -0700

    Check if an integer is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * actual_prime_number - This function recursively calculates
+ * if a number is prime recursively
+ * @n: number to check
+ * @i: iterator
+ *
+ * Return: 1 if 'n' is prime, 0 if 'n' is not
+ */
+
+int actual_prime_number(int n, int i)
+{
+	if (i == 1)
+		return (1);
+
+	else if (n % i == 0 && i > 0)
+		return (0);
+
+	return (actual_prime(n, i - 1));
+}
+
+/**
+ * is_prime_number - This function checks if an integer
+ * is a prime number or not
+ * @n: number to check
+ *
+ * Return: 1 if 'n' is a prime number, 0 if 'n' is not
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (actual_prime(n, n - 1));
+}
