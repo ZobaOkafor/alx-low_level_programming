795e72201cc708895fd90b87298d0973433c9702
ZobaOkafor <judithokafor30@gmail.com>
2023-09-28 12:04:37 -0700

    Print a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,13 +11,12 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		return;
 	}
 
 	else
 	{
 		_putchar(*s);
 		_print_rev_recursion(s + 1);
-		_putchar('\n');
 	}
 }
