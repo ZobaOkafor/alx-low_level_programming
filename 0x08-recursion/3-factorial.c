1ab59110b965aac605e9a92b81625271bab0e5cd
ZobaOkafor <judithokafor30@gmail.com>
2023-09-28 16:28:16 -0700

    Return the factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,13 +6,18 @@
  *
  * Return: factorial of n
  */
+
 int factorial(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
 
 	else if (n == 0)
+	{
 		return (1);
+	}
 
 	return (n * factorial(n - 1));
 }
