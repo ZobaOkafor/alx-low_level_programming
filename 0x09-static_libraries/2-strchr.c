9258dc8773b3650c039bafc2994546f942cd1abd
ZobaOkafor <judithokafor30@gmail.com>
2023-10-02 00:20:08 -0700

    Static library

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - This function locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: s if character is  matched
+ * or NULL if character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+
+	s++;
+	}
+
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
