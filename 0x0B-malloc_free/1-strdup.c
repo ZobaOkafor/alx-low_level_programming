f41826fb6996b227e53d162c9417b7dd9faff12d
ZobaOkafor <judithokafor30@gmail.com>
2023-10-04 05:25:37 -0700

    Return a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -33,19 +33,3 @@ char *_strdup(char *str)
 
 	return (d);
 }
-
-
-int main(void)
-{
-	char *s;
-
-	s = _strdup("ALX SE");
-	if (s == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-	return (0);
-}
