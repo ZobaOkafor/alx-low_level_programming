b59ec06453189a1b72bf4048f8ebd1b0d89a741a
ZobaOkafor <judithokafor30@gmail.com>
2023-10-10 04:25:43 -0700

    Print the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ #define __FILE__
+
+/** 
+ * main - This program prints the name of the file it was compiled from
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	char filename = "2-main.c"
+	
+	printf("%s\n", filename);
+
+	return (0);
+}
