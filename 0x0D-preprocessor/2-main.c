e751c4a7e36c31a8105cc34cfd5f4ef683673e2c
ZobaOkafor <judithokafor30@gmail.com>
2023-10-10 04:34:50 -0700

    Print the name of the file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define __FILE__ 2-main.c
+#define __FILE__
 
-/** 
+/**
  * main - This program prints the name of the file it was compiled from
  *
  * Return: 0 Always
