67f639b79b04036469b8f5ec4e40b14677d684e2
ZobaOkafor <judithokafor30@gmail.com>
2023-10-10 14:01:37 -0700

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,10 +18,19 @@ struct dog
 
 
 /**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+
+/**
  * function prototypes
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
