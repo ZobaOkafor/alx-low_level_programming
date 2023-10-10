b10b13327d841bac73cbf72d3b3d8f1ae9d7411b
ZobaOkafor <judithokafor30@gmail.com>
2023-10-10 11:43:57 -0700

    Header file defining new struct type with accompanying elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - Defining a new struct type
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 }
+
+#endif
