8b5b0bbeff7b201f19251eb80a5fe8d7a59d1c28
ZobaOkafor <judithokafor30@gmail.com>
2023-10-10 15:16:08 -0700

    Initialize a variable type of struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,6 @@ struct dog
  * function prototypes
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
