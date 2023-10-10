887250307dd2d562e6841983f75e655156480caa
ZobaOkafor <judithokafor30@gmail.com>
2023-10-10 14:48:58 -0700

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - This defines a new type 'struct dog'
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ */
+
 struct dog
 {
 	char *name;
