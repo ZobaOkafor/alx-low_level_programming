6d1ed012b4494d2a821156f1547ea77a210cb2c4
ZobaOkafor <judithokafor30@gmail.com>
2023-10-12 13:48:19 -0700

    Header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VAR_FUNCTIONS_H
+#define VAR_FUNCTIONS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
