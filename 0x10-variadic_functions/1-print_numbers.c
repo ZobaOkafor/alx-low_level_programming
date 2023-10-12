d7b13d7a675ffc9e9a0a7118fa89aee533788366
ZobaOkafor <judithokafor30@gmail.com>
2023-10-12 16:27:08 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		num = va_arg(integers, int);
 		printf("%d", num);
 
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 
