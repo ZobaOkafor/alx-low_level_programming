2447a392f87f41c6a49ecac9f65d01e02397db45
ZobaOkafor <judithokafor30@gmail.com>
2023-10-12 17:03:20 -0700

    Print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -35,15 +35,3 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_end(strings);
 }
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_strings(", ", 2, "Jay", "Django");
-	return (0);
-}
