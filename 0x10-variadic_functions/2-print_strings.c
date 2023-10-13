628e59330963e2c20c351916dd2187b473807f60
ZobaOkafor <judithokafor30@gmail.com>
2023-10-12 17:26:07 -0700

    Print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,9 +30,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (i < n - 1 && separator != NULL)
 			printf("%s", str);
-
-		else
-			printf("%s%s", separator, str);
 	}
 
 	printf("\n");
