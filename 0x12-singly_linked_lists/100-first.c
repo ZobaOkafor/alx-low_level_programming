be64f044067e0a35e66b24839dfd8cf52809b724
ZobaOkafor <judithokafor30@gmail.com>
2023-10-19 17:27:29 -0700

    Header file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints a string before the main function is executed
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
