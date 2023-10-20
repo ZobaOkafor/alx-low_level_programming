cb2d13ccc6d79d8679db0a7a5394dceec8b5fb6c
ZobaOkafor <judithokafor30@gmail.com>
2023-10-19 17:29:43 -0700

    Print a string before main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+
 	return (0);
 }
