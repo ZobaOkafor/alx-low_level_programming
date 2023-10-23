76a6ba2189c59b56327dd59d0f2e3f5f7e611546
ZobaOkafor <judithokafor30@gmail.com>
2023-10-23 15:03:20 -0700

    Print a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,45 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop - This function finds the loop in a linked list
+ * @head: pointer to the list
+ *
+ * Return: the address of the node where the loop starts,
+ * or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *faster = head;
+	listint_t *slower = head;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (slower && faster && faster->next)
+	{
+		slower = slower->next;
+		faster = faster->next->next;
+
+		if (slower == faster)
+		{
+			slower = head;
+
+			while (slower != faster)
+			{
+				slower = slower->next;
+				faster = faster->next;
+			}
+
+			return (slower);
+		}
+	}
+
+	return (NULL);
+}
+
+
+/**
  * print_listint_safe - This function prints a linked list
  * @head: pointer to the list
  *
