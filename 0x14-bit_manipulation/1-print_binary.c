388eae3b692b242bd66b0aefaa4f681ad34ccf46
ZobaOkafor <judithokafor30@gmail.com>
2023-10-26 13:53:28 -0700

    Print the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -44,5 +44,19 @@ void print_binary(unsigned long int n)
 
 		i = i >> 1;
 	}
-	_putchar('\n');
+}
+
+int main(void)
+{
+    print_binary(0);
+    printf("\n");
+    print_binary(1);
+    printf("\n");
+    print_binary(98);
+    printf("\n");
+    print_binary(1024);
+    printf("\n");
+    print_binary((1 << 10) + 1);
+    printf("\n");
+    return (0);
 }
