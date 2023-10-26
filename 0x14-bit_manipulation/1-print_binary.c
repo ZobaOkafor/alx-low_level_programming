17df2b8b8b48e82572b5e2782bf53bac1a2e2da1
ZobaOkafor <judithokafor30@gmail.com>
2023-10-26 13:43:33 -0700

    _putchar file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+
+/**
+ * print_binary - This function prints the binary representation of a number
+ * @n: number to print
+ *
+ * Return: converted number
+ * or 0 if there are chars not 0 or 1 in the string or if 'b' is NULL
+ */
+
+void print_binary(unsigned long int n)
+{
+	int b, i = 1, j = 0;
+	int num_bits = 0;
+	int tmp;
+
+	tmp = n;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return;
+	}
+	
+	while (tmp > 0)
+	{
+		num_bits++;
+		tmp = tmp >> 1;
+	}
+
+	i = 1 << (num_bits - 1);
+
+	while (i > 0)
+	{
+		b = (n & i) != 0;
+		
+		if (b == 1 || j == 1)
+			_putchar(b + '0');
+
+		if (b == 1)
+			j = 1;
+
+		i = i >> 1;
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+    print_binary(0);
+    printf("\n");
+    print_binary(1);
+    printf("\n");
+    print_binary(98);
+    printf("\n");
+    print_binary(1024);
+    printf("\n");
+    print_binary((1 << 10) + 1);
+    printf("\n");
+    return (0);
+}
