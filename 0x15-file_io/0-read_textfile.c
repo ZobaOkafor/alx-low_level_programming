10269f6d0823d0c2e3c62c57976b5c783aaa5fa1
ZobaOkafor <judithokafor30@gmail.com>
2023-10-30 08:46:48 -0700

    Read a text file and print it to POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,58 +11,26 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
-	
-	buffer = malloc(sizeof(char) * letters);
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	FILE *fp;
+	char ch;
+	size_t i = 0;
+	int count = 0;
 
-	if (filename == NULL)
+	fp = fopen(filename, "r");
+	if (fp == NULL)
 		return (0);
 
-	if (buffer == NULL)
-		return (0);
-
-	if (o == -1)
+	while ((ch = fgetc(fp)) != EOF)
 	{
-		free(buffer);
-		return (0);
+		i++;
+		if (i <= letters)
+		{
+			_putchar(ch);
+			count++;
+		}
 	}
 
-	if (r == -1)
-	{
-		free(buffer);
-		close(o);
-		return (0);
-	}
+	fclose(fp);
 
-	if (w == -1 || w != r)
-	{
-		free(buffer);
-		close(o);
-		return (0);
-	}
-
-	free(buffer);
-	close(o);
-	
-	return (w);
-}
-
-int main(int ac, char **av)
-{
-    ssize_t n;
-
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+	return (count);
 }
