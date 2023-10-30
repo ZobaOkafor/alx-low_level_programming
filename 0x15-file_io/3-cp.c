18dbe32a63cacce15b3ec7e9e510f6972bcb94dd
ZobaOkafor <judithokafor30@gmail.com>
2023-10-30 16:15:06 -0700

    Copy the contents of one file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,22 +4,24 @@
 /**
  * error_exit - This function exits the program with an error message
  * and an exit code
- * @ex_code: the exit code to return when program exits
- * @format: string for the error message
+ * @file_from: the exit code to return when program exits
+ * @file_to: string for the error message
+ * @argv: pointer
  *
  */
 
-void error_exit(int ex_code, const char *format, ...)
+void error_exit(int file_from, int file_to, char *argv[])
 {
-	va_list err;
-
-	va_start(err, format);
-
-	dprintf(2, format, err);
-
-	va_end(err);
-
-	exit(ex_code);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
 
@@ -38,39 +40,39 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n");
-
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	if (file_to == -1)
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-
 	nchars = 1024;
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+		}
 	}
-
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
-
 	return (0);
 }
