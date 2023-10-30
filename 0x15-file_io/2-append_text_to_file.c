089cbe9b98e716649105c801e6db6395a9b7b685
ZobaOkafor <judithokafor30@gmail.com>
2023-10-30 12:58:59 -0700

    Append text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,39 +10,25 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int op, wr;
+	int op, wr;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        op = open(filename, O_WRONLY | O_APPEND);
-        if (op == -1)
-                return (-1);
+	op = open(filename, O_WRONLY | O_APPEND);
+	if (op == -1)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                wr = write(op, text_content, strlen(text_content));
-                if (wr == -1)
-                {
-                        close(op);
-                        return (-1);
-                }
-        }
+	if (text_content != NULL)
+	{
+		wr = write(op, text_content, strlen(text_content));
+		if (wr == -1)
+		{
+			close(op);
+			return (-1);
+		}
+	}
 
-        close(op);
-        return (1);
-}
-
-int main(int ac, char **av)
-{
-    int res;
-
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	close(op);
+	return (1);
 }
