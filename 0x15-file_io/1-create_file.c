68dea0b7d9d453c0c1c8ddb5a4019707ed71c79c
ZobaOkafor <judithokafor30@gmail.com>
2023-10-30 12:34:16 -0700

    Create a file with rw permission and append string

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - This function creates a file
+ * @filename: name of file to create
+ * @text_content: name of null terminating dtring to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (op == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		wr = write(op, text_content, strlen(text_content));
+		if (wr == -1)
+		{
+			close(op);
+			return (-1);
+		}
+	}
+
+	close(op);
+	return (1);
+}
