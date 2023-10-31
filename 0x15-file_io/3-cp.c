add71c9f9b7c8308bf3cf4a6b53082cf450335cc
ZobaOkafor <judithokafor30@gmail.com>
2023-10-30 18:02:12 -0700

    Copy the contents of one file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,33 +33,32 @@ void error_exit(int file_from, int file_to, char *argv[])
  * Return: 0 on success
  */
 
-
 int main(int argc, char *argv[])
 {
-	int eclose, file_from, file_to;
-	ssize_t rd, wr;
+	int file_from, file_to, eclose;
+	ssize_t wr, nch;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to %s\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	rd = 1024;
-	while (rd == 1024)
+	nch = 1024;
+	while (nch == 1024)
 	{
-		rd = read(file_from, buffer, 1024);
-		if (rd == -1)
+		nch = read(file_from, buffer, 1024);
+		if (nch == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		wr = write(file_to, buffer, rd);
+		wr = write(file_to, buffer, nch);
 		if (wr == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
