14161e542602376feaa0b01fe7421b1322c91fec
ZobaOkafor <judithokafor30@gmail.com>
2023-10-30 17:23:01 -0700

    Copy the contents of one file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -61,7 +61,6 @@ int main(int argc, char *argv[])
 {
 	char *file_from = argv[1];
 	char *file_to = argv[2];
-	int fd_from, fd_to;
 	ssize_t rd, wr;
 	char buffer[1024];
 
@@ -70,27 +69,19 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
+	file_from = open(file_from, O_RDONLY);
+	file_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	rd = 1024;
+	while (rd == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-		exit(99);
-	}
-	while ((rd = read(fd_from, buffer, sizeof(buffer))) > 0)
-	{
+		rd = read(file_from, buffer, 1024)
 		if (rd == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
 
-		wr = write(fd_to, buffer, rd);
+		wr = write(file_to, buffer, rd);
 		if (wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
