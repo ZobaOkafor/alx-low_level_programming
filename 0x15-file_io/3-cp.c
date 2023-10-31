c3bda578f95ee04fcb02533e23a1df758cb1e43d
ZobaOkafor <judithokafor30@gmail.com>
2023-10-30 17:01:00 -0700

    Copy the contents of one file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -71,17 +71,8 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-		exit(99);
-	}
+
 	while ((rd = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		if (rd == -1)
