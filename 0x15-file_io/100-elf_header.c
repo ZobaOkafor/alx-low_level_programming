e7bd62218dc2a2382d6c32aaf078f1c9c76fc42f
ZobaOkafor <judithokafor30@gmail.com>
2023-10-31 06:34:58 -0700

    Display the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -198,7 +198,7 @@ void handle_entry_point_addr(unsigned long int e_entry, unsigned char *p)
 	if (p[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
-			((e_entry >> 8) & 0xFF00FF);
+			((e_entry >> 8) & 0x00FF00FF);
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 
