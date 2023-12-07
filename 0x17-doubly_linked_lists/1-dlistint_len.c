02469c871ba3956f544ec72517befee412a3cd45
ZobaOkafor <judithokafor30@gmail.com>
2023-12-07 09:59:36 -0800

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * dlistint_len - This function returns the number of elements in a linked list
  * @h: pointer to the head of the list
  *
- * Return: the number of elements 
+ * Return: the number of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
