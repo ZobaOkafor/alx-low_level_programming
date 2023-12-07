8edcd2fbaccae8168e00df56924540c00e9e5bc7
ZobaOkafor <judithokafor30@gmail.com>
2023-12-07 09:34:50 -0800

    Header file

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,36 @@
+#ifndef lists_h
+#define lists_h
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+
+int sum_dlistint(dlistint_t *head);
+void free_dlistint(dlistint_t *head);
+size_t dlistint_len(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+
+#endif
