8ec37f403fd3953eb71aeccd1c22c3c9a86caeee
ZobaOkafor <judithokafor30@gmail.com>
2023-12-22 03:49:45 -0800

    Create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_create - This function creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table
+ * or NULL if something went wrong.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *ht = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	ht->size = size;
+
+	return (ht);
+}
+
+
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
+}
