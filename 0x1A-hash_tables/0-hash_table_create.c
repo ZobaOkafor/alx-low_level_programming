dd8f92e366183a297064881aa81442863d62c583
ZobaOkafor <judithokafor30@gmail.com>
2023-12-22 11:15:09 -0800

    Create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,20 +11,27 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
 	unsigned long int i;
+	hash_table_t *ht = NULL;
+
+	if (size == 0)
+		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
+	{
+		free(ht);
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
+
+	ht->size = size;
 
 	return (ht);
 }
