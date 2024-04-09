db8ae26887d9c71e5f7226d5913004aebd71cd0c
ZobaOkafor <judithokafor30@gmail.com>
2024-04-09 12:38:47 -0700

    Advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -71,22 +71,3 @@ int advanced_binary(int *array, size_t size, int value)
 
 	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
-
-
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
-{
-	int array[] = {
-		0, 1, 2, 5, 5, 6, 6, 7, 8, 9
-	};
-	size_t size = sizeof(array) / sizeof(array[0]);
-
-	printf("Found %d at index: %d\n\n", 8, advanced_binary(array, size, 8));
-	printf("Found %d at index: %d\n\n", 5, advanced_binary(array, size, 5));
-	printf("Found %d at index: %d\n", 999, advanced_binary(array, size, 999));
-	return (EXIT_SUCCESS);
-}
