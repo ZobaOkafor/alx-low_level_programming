7387d9b191a5d67ccc1778cc4f39cec51f3e787f
ZobaOkafor <judithokafor30@gmail.com>
2024-04-10 07:24:13 -0700

    Jump search in a singly linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -24,15 +24,18 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		for (i = 0; i < jump && current->next != NULL; i++)
 			current = current->next;
 
-		printf("Value checked at index [%d] = [%d]\n", (int)current->index, current->n);
+		printf("Value checked at index [%d] = [%d]\n",
+			(int)current->index, current->n);
 
 		if (current->n >= value || current->next == NULL)
 			break;
 	} while (current != NULL);
 
-	printf("Value found between indexes [%d] and [%d]\n", (int)prev->index, (int)current->index);
+	printf("Value found between indexes [%d] and [%d]\n",
+		(int)prev->index, (int)current->index);
 
-	for (temp = prev; temp != NULL && temp->index <= current->index; temp = temp->next)
+	for (temp = prev; temp != NULL && temp->index <= current->index;
+		temp = temp->next)
 	{
 		printf("Value checked at index [%d] = [%d]\n", (int)temp->index, temp->n);
 		if (temp->n == value)
